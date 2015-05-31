92b1ded5a1c0491a5ef8610902c8d1051a0c1ec0
Pantham Rahul <rahul@Panthams-MacBook-Pro.local>
2015-05-31 13:37:51 -0400

    Initial Commit

---
+++ DateCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DateCalculator
+//
+//  Created by Pantham Rahul on 5/31/15.
+//  Copyright (c) 2015 Pantham Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
